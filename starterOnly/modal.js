ee68325c23103cc96e019b2fc3c9248f9a8d495a
emmanuel tulinayo <oyan.tuli@gmail.com>
2023-07-07 08:52:20 +0200

    fermeture_modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// 
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +7,38 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
+// 
 // DOM Elements
+const closeBtn = document.querySelector("span.close")
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
+// const formData = document.querySelectorAll(".formData");
+const form = document.querySelector('[name="reserve"]')
+// 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+// formData.forEach((field) => field.addEventListener("inputs", ))
+closeBtn.addEventListener("click", toggleModal)
+form.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  // console.log()
+  // console.log(form)
+  console.log("test--validation")
+  
+})
+// 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+//
+function toggleModal() {
+  modalbg.style.display = "none"
+  console.log("test--closeModal")
+}
+// 
+// function validateForm() {
+//   if()
+//   console.log("test--validation")
+// }
+// 
