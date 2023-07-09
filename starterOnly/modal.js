1443eeaee8e603539df4626dcbf5c8dc0f265095
emmanuel tulinayo <oyan.tuli@gmail.com>
2023-07-09 18:40:28 +0200

    radio_css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,19 +12,59 @@ function editNav() {
 const closeBtn = document.querySelector("span.close")
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-// const formData = document.querySelectorAll(".formData");
+const formDivs = document.querySelectorAll(".formData");
 const form = document.querySelector('[name="reserve"]')
+// 
+function formFieldError(errMsg, fieldNum) {
+  // var span = document.querySelector(`form[name="reserve"]>div:nth-child(${fieldNum + 1})>span`)
+  // if (!span || span.getAttribute("data-error") === undefined) {
+  if (
+    formDivs[fieldNum].getAttribute("data-error") === null ||
+    formDivs[fieldNum].getAttribute("data-error") === undefined ) {
+    // var span = document.createElement("span")
+    formDivs[fieldNum].setAttribute("data-error", errMsg)
+    // formDivs[fieldNum].setAttribute("data-error-visible", "true")
+    // formDivs[fieldNum].appendChild(span)
+    // console.log(formDivs[fieldNum].getAttribute("data-error-visible"))
+    console.log(formDivs[fieldNum].getAttribute("data-error"))
+  } 
+  else {
+    console.log(formDivs[fieldNum].getAttribute("data-error"))
+    formDivs[fieldNum].setAttribute("data-error-vsible", "false")
+    // console.log("span already exists.")
+  }
+}
 // 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// formData.forEach((field) => field.addEventListener("inputs", ))
-closeBtn.addEventListener("click", toggleModal)
+closeBtn.addEventListener("click", closeModal)
 form.addEventListener("submit", (e)=>{
   e.preventDefault()
+  const fData = new FormData(form)
+  // const formObj = 
+  if (!fData.get("first")) {
+    formFieldError("veuillez entrer 2 caractères ou plus pour le champs du prénom", 0 )
+  }
+  if (!fData.get("last")) {
+    formFieldError("veuillez entrer 2 caractères ou plus pour le champs du nom.", 1 )
+  }
+  if (!fData.get("email")) {
+    formFieldError("veuillez entrer un e-mail valide.", 2 )
+  }
+  if (!fData.get("birthdate")) {
+    formFieldError("vous devez entrer votre date de naissance", 3 )
+  }
+  if (isNaN(parseInt(fData.get("quantity")))) {
+    formFieldError("vous devez entrer un nombre.", 4 )
+  }
+  if (!fData.get("location")) {
+    formFieldError("vous devez choisir une option.", 5 )
+  }
+  if (!fData.get("location")) {
+    formFieldError("vous devez choisir une option.", 5 )
+  }
   // console.log()
-  // console.log(form)
-  console.log("test--validation")
-  
+  // console.log("test--validation")
 })
 // 
 // launch modal form
@@ -32,13 +72,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 //
-function toggleModal() {
+function closeModal() {
   modalbg.style.display = "none"
   console.log("test--closeModal")
 }
 // 
-// function validateForm() {
-//   if()
-//   console.log("test--validation")
-// }
-// 
+
